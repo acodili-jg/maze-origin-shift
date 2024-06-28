6f4a2bafdb4ee0099a791d343bf6427c1e896f5b
Jack Gabriel C. Acodili <112671361+acodili-jg@users.noreply.github.com>
2024-06-28 19:14:32 +0800

    Add rand and implement for Direction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
 use std::io::{self, stdout, Write};
 
 fn main() -> io::Result<()> {
@@ -12,8 +14,9 @@ fn writeln_maze<const W: usize, const H: usize, O: Write>(
 ) -> io::Result<()> {
     for y in 0..H {
         for x in 0..W {
+            #[allow(clippy::expect_used)]
             let ch = format_maze_node(it.get(x, y).expect("node in bounds"));
-            write!(out, "{ch}")?;
+            write!(out, " {ch} ")?;
         }
         writeln!(out)?;
     }
@@ -21,19 +24,20 @@ fn writeln_maze<const W: usize, const H: usize, O: Write>(
 }
 
 #[inline]
-const fn format_maze_node(it: MazeNode) -> &'static str {
+const fn format_maze_node(it: MazeNode) -> char {
     match it.direction() {
-        None => " · ",
-        Some(Direction::Left) => " ← ",
-        Some(Direction::Up) => " ↑ ",
-        Some(Direction::Right) => " → ",
-        Some(Direction::Down) => " ↓ ",
+        None => '·',
+        Some(Direction::Left) => '←',
+        Some(Direction::Up) => '↑',
+        Some(Direction::Right) => '→',
+        Some(Direction::Down) => '↓',
     }
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct MazeGraph<const W: usize, const H: usize = W> {
     data: [[MazeNode; W]; H],
+    origin: (usize, usize),
 }
 
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
@@ -49,6 +53,19 @@ enum Direction {
     Down,
 }
 
+impl Direction {
+    #[inline]
+    pub const fn from_ordinal(ordinal: usize) -> Result<Self, usize> {
+        Ok(match ordinal {
+            0 => Self::Left,
+            1 => Self::Up,
+            2 => Self::Right,
+            3 => Self::Down,
+            ordinal => return Err(ordinal),
+        })
+    }
+}
+
 impl<const W: usize, const H: usize> MazeGraph<W, H> {
     #[inline]
     pub fn get(self, x: usize, y: usize) -> Option<MazeNode> {
@@ -58,6 +75,21 @@ impl<const W: usize, const H: usize> MazeGraph<W, H> {
 
 impl MazeNode {
     #[inline]
+    pub const fn new(direction: Option<Direction>) -> Self {
+        Self { direction }
+    }
+
+    #[inline]
+    pub const fn new_origin() -> Self {
+        Self::new(None)
+    }
+
+    #[inline]
+    pub const fn new_towards(direction: Direction) -> Self {
+        Self::new(Some(direction))
+    }
+
+    #[inline]
     pub const fn direction(self) -> Option<Direction> {
         self.direction
     }
@@ -66,26 +98,45 @@ impl MazeNode {
 impl<const W: usize, const H: usize> Default for MazeGraph<W, H> {
     #[inline]
     fn default() -> Self {
-        let mut row = [Direction::Left.into(); W];
-        row[0] = Direction::Up.into();
+        #[inline]
+        const fn default_data<const W: usize, const H: usize>() -> [[MazeNode; W]; H] {
+            #[inline]
+            const fn default_row<const W: usize>() -> [MazeNode; W] {
+                let mut row = [MazeNode::new_towards(Direction::Left); W];
+                row[0] = MazeNode::new_towards(Direction::Up);
+                row
+            }
 
-        let mut data = [row; H];
-        data[0][0] = MazeNode::default();
+            let mut data = [default_row(); H];
+            data[0][0] = MazeNode::new_origin();
+            data
+        }
 
-        Self { data }
+        let data = default_data();
+        let origin = Default::default();
+
+        Self { data, origin }
+    }
+}
+
+impl Distribution<Direction> for Standard {
+    #[inline]
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        #[allow(clippy::expect_used)]
+        Direction::from_ordinal(rng.gen_range(0..4)).expect("ordinal is valid")
     }
 }
 
 impl From<Direction> for MazeNode {
     #[inline]
     fn from(direction: Direction) -> Self {
-        Some(direction).into()
+        Self::new_towards(direction)
     }
 }
 
 impl From<Option<Direction>> for MazeNode {
     #[inline]
     fn from(direction: Option<Direction>) -> Self {
-        Self { direction }
+        Self::new(direction)
     }
 }
